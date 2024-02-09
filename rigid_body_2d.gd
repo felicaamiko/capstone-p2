796ab8fcd398041140fc963f16d438c9ff3c0acc
clement lee <felicxaamiko@gmail.com>
2024-02-09 15:05:43 -0600

    bad ver

--- rigid_body_2d.gd
+++ rigid_body_2d.gd
@@ -1,45 +1,35 @@
 extends RigidBody2D
 
 var isHovered = false
-var selected = false
 var mouseoffset = Vector2(0,0)
 var firstframe = true
 var prevmousepos = Vector2(0,0)
 var mousepos = Vector2(0,0)
 var lastvector = Vector2(0,0)
 var lastvectorcopy = Vector2(0,0)
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _ready():
+	pass
+
 func _physics_process(delta):
-	#if Input.is_action_just_pressed("click"):
-		#mouseoffset = position - get_global_mouse_position()
 	prevmousepos = mousepos
 	mousepos = get_global_mouse_position()
 	lastvector = (mousepos-prevmousepos)
-	#print_debug(lastvector)
-	if (isHovered == true and Input.is_action_pressed("click")):#held down
-		if (firstframe):
-			mouseoffset = position - get_global_mouse_position()
-			firstframe = false
-		#print_debug("held down")
-		position = get_global_mouse_position() + mouseoffset
+	
+	
+	if (isHovered and Input.is_action_pressed("click")):#held down
+		apply_impulse(position, lastvector)
+		#if (firstframe):
+			#mouseoffset = position - get_global_mouse_position()
+			#firstframe = false
+		#position = get_global_mouse_position() + mouseoffset
 		prevmousepos = get_global_mouse_position()
 	else:
 		if (not firstframe):
 			linear_velocity = lastvector
-			#lastvectorcopy = lastvector
-			#position += lastvectorcopy
-			#position += lastvectorcopy #changing to a - makes cool slingshot effect.
 			lastvectorcopy *= .5
 			if (lastvectorcopy == Vector2(0,0)):
 				firstframe = true
-		#lastvectorcopy *= .75
-		#position += lastvectorcopy #changing to a - makes cool slingshot effect.
-		#print_debug(lastvectorcopy)
-		#position += (prevmousepos-(get_global_mouse_position()))
 
 func _on_area_2d_mouse_entered():
 	print_debug("entered")

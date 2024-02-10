efed187ddf034a7899fa4a93ff000c539ab4c642
clement lee <felicxaamiko@gmail.com>
2024-02-10 16:20:39 -0600

    sucks but works a bit?

--- rigid_body_2d.gd
+++ rigid_body_2d.gd
@@ -6,23 +6,32 @@ var firstframe = true
 var prevmousepos = Vector2(0,0)
 var mousepos = Vector2(0,0)
 var lastvector = Vector2(0,0)
-var lastvectorcopy = Vector2(0,0)
-var sensitivity = 10000
+var sqrtsenslastvector = Vector2(0,0)
+var sensitivity = 500
+var horizdir = 1 #returns 1 if headed right, -1 if headed left
+var vertdir = 1 #returns 1 if headed up, -1 if headed down
 
 var untilsixty = 0
-
 
 func _ready():
 	pass
 
 func _physics_process(delta):
+	if lastvector.x < 0:
+		horizdir = -1
+	else:
+		horizdir = 1
+	
+	if lastvector.y < 0:
+		vertdir = -1
+	else: vertdir = 1
 	prevmousepos = mousepos
 	mousepos = get_global_mouse_position()
 	lastvector = (mousepos-prevmousepos)
-	print_debug(lastvector)
-	
+	print_debug(lastvector.x)
+	sqrtsenslastvector = Vector2(sqrt(abs(lastvector.x)) * horizdir, sqrt(abs(lastvector.y)) * vertdir) * sensitivity
 	if (isHovered and Input.is_action_pressed("click") and firstframe == true):#held down first frame
-		apply_force(lastvector * sensitivity, Vector2(0,0))
+		apply_force(sqrtsenslastvector, Vector2(0,0))
 		#apply_impulse(lastvector, position)
 		untilsixty += 1
 		#if (untilsixty > 60):

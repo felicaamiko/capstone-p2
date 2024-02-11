58cf84f624988d639cf439dfb942d55e8eb7336b
clement lee <felicxaamiko@gmail.com>
2024-02-11 16:20:47 -0600

    click follow

--- rigid_body_2d.gd
+++ rigid_body_2d.gd
@@ -10,45 +10,44 @@ var sqrtsenslastvector = Vector2(0,0)
 var sensitivity = 500
 var horizdir = 1 #returns 1 if headed right, -1 if headed left
 var vertdir = 1 #returns 1 if headed up, -1 if headed down
-
+var clicked = false
 var untilsixty = 0
 
 func _ready():
 	pass
 
+#get mouse position vs get global mouse pos
+
+#func _integrate_forces(state: PhysicsDirectBodyState2D):  #can go to sleep
+	#print(get_viewport().get_mouse_position())
+	#if (isHovered and clicked):
+		#state.transform.origin = get_viewport().get_mouse_position()
+
+
 func _physics_process(delta):
+	clicked = Input.is_action_pressed("click")
 	if lastvector.x < 0:
 		horizdir = -1
 	else:
 		horizdir = 1
-	
+
 	if lastvector.y < 0:
 		vertdir = -1
 	else: vertdir = 1
+
 	prevmousepos = mousepos
 	mousepos = get_global_mouse_position()
 	lastvector = (mousepos-prevmousepos)
-	#print_debug(lastvector.x)
 	sqrtsenslastvector = Vector2(sqrt(abs(lastvector.x)) * horizdir, sqrt(abs(lastvector.y)) * vertdir) * sensitivity
-	if (isHovered and Input.is_action_pressed("click") and firstframe == true):#held down first frame
+	if (isHovered and Input.is_action_pressed("click")):#held down first frame
 		apply_force(sqrtsenslastvector, Vector2(0,0))
-		print_debug(transform.get_rotation())#rotation is in radians
-		#apply_impulse(lastvector, position)
-		untilsixty += 1
-		#if (untilsixty > 60):
-			#firstframe = false
-			#untilsixty = 0
-	else:
-		if (not firstframe):
-			#linear_velocity = lastvector"linear_damp_mode"
-			if (lastvector == Vector2(0,0)):
-				firstframe = true
-		#pass
 
 func _on_area_2d_mouse_entered():
-	print_debug("entered")
+	#print_debug("entered")
 	isHovered = true
+	
+	#apply_force(Vector2(0, 0), position)
 
 func _on_area_2d_mouse_exited():
-	print_debug("exited")
+	#print_debug("exited")
 	isHovered = false
